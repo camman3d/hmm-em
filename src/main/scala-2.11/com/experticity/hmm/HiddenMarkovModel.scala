243fc0532a5973959eab0393678258a1e5e397b6
Josh Monson <Josh.Monson@experticity.com>
2016-04-15 10:56:26 -0600

    Working through example

--- src/main/scala-2.11/com/experticity/hmm/HiddenMarkovModel.scala
+++ src/main/scala-2.11/com/experticity/hmm/HiddenMarkovModel.scala
@@ -1,42 +1,169 @@
 package com.experticity.hmm
 
 import breeze.linalg.{DenseMatrix, DenseVector}
-import com.experticity.hmm.HiddenMarkovModel.Observation
 
-case class HiddenMarkovModel(initial: DenseVector[Double], transitions: DenseMatrix[Double], emissions: DenseMatrix[Double], sequence: List[Observation]) {
-  def withSequence(sequence: List[Observation]) = copy(sequence = sequence)
+import scala.collection.mutable.ListBuffer
+import scala.util.Random
 
+case class HMMMeta(numStates: Int, numObservations: Int)
+
+case class HiddenMarkovModel(meta: HMMMeta, initial: DenseVector[Double], transitions: DenseMatrix[Double], emissions: DenseMatrix[Double], sequence: List[Int] = List()) {
+  // To make it easier to match equations
+  val (a, b, pi, y) = (transitions, emissions, initial, sequence)
+
+  def generateSequence(length: Int) = {
+    def next(v: DenseVector[Double]) = {
+      val r = Random.nextDouble()
+      (0 until v.length).find(i => {
+        val low = v(0 until i).sum
+        val high = low + v(i)
+        r >= low && r < high
+      }).get
+    }
+
+    val states = new ListBuffer[Int]()
+    states += next(initial)
+    (1 until length).foreach(i => {
+      states += next(transitions(states(i-1),::).t)
+    })
+
+    val observations = states.map(state => {
+      next(emissions(state,::).t)
+    })
+
+    (observations.toList, states.toList)
+  }
+
+  def withSequence(sequence: List[Int]) = copy(sequence = sequence)
+
+  def initialGuess() = {
+    val stateP = 1.0 / meta.numStates.toDouble
+    val obserP = 1.0 / meta.numObservations.toDouble
+
+    val initial = DenseVector.fill(meta.numStates, stateP)
+    val transitions = DenseMatrix.fill(meta.numStates, meta.numStates)(stateP)
+    val emissions = DenseMatrix.fill(meta.numStates, meta.numObservations)(obserP)
+    copy(initial = initial, transitions = transitions, emissions = emissions)
+  }
+
+  def forT(f: Int => Unit) = sequence.indices.foreach(f)
+  def `forT-1`(f: Int => Unit) = (0 until sequence.size-1).foreach(f)
+  def sumT(f: Int => Double) = (0 until sequence.size).map(f).sum
+  def `sumT-1`(f: Int => Double) = (0 until sequence.size-1).map(f).sum
+  def fromT(f: Int => Unit) = sequence.indices.reverse.foreach(f)
+  def forN(f: Int => Unit) = (0 until meta.numStates).foreach(f)
+  def sumN(f: Int => Double) = (0 until meta.numStates).map(f).sum
+  def forM(f: Int => Unit) = (0 until meta.numObservations).foreach(f)
+
+  def alpha() = {
+    val alpha = DenseMatrix.zeros[Double](meta.numStates, sequence.size)
+
+    // Forward algorithm
+    forT(t => {
+      forN(i => {
+        if (t == 0) alpha(i, t) = pi(i) * b(i, y(t))
+        else        alpha(i, t) = b(i, y(t)) * sumN(j => alpha(j, t-1) * a(j, i))
+      })
+    })
+
+    alpha
+  }
+
+  def beta() = {
+    val beta = DenseMatrix.zeros[Double](meta.numStates, sequence.size)
+
+    // Backward algorithm
+    fromT(t => {
+      forN(i => {
+        if (t == sequence.size - 1) beta(i, t) = 1
+        else                        beta(i, t) = sumN(j => beta(j, t+1) * a(i,j) * b(j, y(t+1)))
+      })
+    })
+
+    beta
+  }
+
+  def gamma(alpha: DenseMatrix[Double], beta: DenseMatrix[Double]) = {
+    val gamma = DenseMatrix.zeros[Double](meta.numStates, sequence.size)
+
+    forT(t => {
+      forN(i => {
+        gamma(i, t) = (alpha(i,t) * beta(i,t)) / sumN(j => alpha(j,t) * beta(j,t))
+//        gamma(i, t) = (alpha(i,t) * beta(i,t)) / sumN(j => alpha(j,t) * beta(j,t))
+      })
+    })
+
+    gamma
+  }
+
+  def xi(alpha: DenseMatrix[Double], beta: DenseMatrix[Double]) = {
+    val xi = (0 until meta.numStates).map(_ => DenseMatrix.zeros[Double](meta.numStates, sequence.size - 1)).toList
+
+    val sum = sumN(k => alpha(k, meta.numStates - 1))
+    `forT-1`(t => {
+      forN(i => {
+        forN(j => {
+          xi(i)(j,t) = (alpha(i,t) * a(i,j) * beta(j,t+1) * b(j, y(t+1))) / sum
+        })
+      })
+    })
+
+    xi
+  }
+
+  def normalize(a: DenseMatrix[Double]) = {
+    val normalized = a.copy
+    forN(i => {
+      val sum = sumN(j => a(i,j))
+      forN(j => normalized(i,j) = a(i,j) / sum)
+    })
+    normalized
+  }
+
+  def update(gamma: DenseMatrix[Double], xi: List[DenseMatrix[Double]]) = {
+    val newPi = gamma(::, 0)
+
+    val newA = DenseMatrix.zeros[Double](meta.numStates, meta.numStates)
+    forN(i => {
+      forN(j => {
+        newA(i,j) = `sumT-1`(t => xi(i)(j,t)) / `sumT-1`(t => gamma(i,t))
+      })
+    })
+
+    val newB = DenseMatrix.zeros[Double](meta.numStates, meta.numObservations)
+    forN(i => {
+      forM(v => {
+        newB(i, v) = sumT(t => if (v == y(t)) gamma(i, t) else 0) / sumT(t => gamma(i, t))
+      })
+    })
+
+    copy(initial = newPi, transitions = normalize(newA), emissions = newB)
+  }
+
+  def emStep() = {
+    val _a = alpha()
+    val _b = beta()
+    val _g = gamma(_a, _b)
+    val _x = xi(_a, _b)
+    update(_g, _x)
+  }
+
+  override def toString: String =
+    s"""
+      |Initial:
+      |${initial}
+      |
+      |Transitions:
+      |${transitions}
+      |
+      |Emissions:
+      |${emissions}
+    """.stripMargin
 }
 
 object HiddenMarkovModel {
 
-  case class State(name: Symbol)
-  case class Observation(name: Symbol)
-
-  implicit def toState(name: Symbol): State = State(name)
-  implicit def toObservation(name: Symbol): Observation = Observation(name)
-
-
-  def initialGuess(states: List[State], observations: List[Observation]) = {
-    val stateP = 1.0 / states.size.toDouble
-    val obserP = 1.0 / observations.size.toDouble
-
-    val initial = DenseVector.fill(states.size, stateP)
-    val transitions = DenseMatrix.fill(states.size, states.size)(stateP)
-    val emissions = DenseMatrix.fill(states.size, observations.size)(obserP)
-
-//    val initial: InitialStateTable = states
-//      .map(s => (s, 1.0 / states.size.toDouble))
-//      .toMap
-//    val transitions: TransitionTable = states
-//      .flatMap(s1 => states.map(s2 => (s1, s2)))
-//      .map(t => (t, 1.0 / states.size.toDouble))
-//      .toMap
-//    val emissions: EmissionTable = states
-//      .flatMap(s => observations.map(o => (o, s)))
-//      .map(e => (e, 1.0 / observations.size.toDouble))
-//      .toMap
-    HiddenMarkovModel(initial, transitions, emissions, List())
-  }
+  def apply(numStates: Int, numObservations: Int): HiddenMarkovModel =
+    HiddenMarkovModel(HMMMeta(numStates, numObservations), DenseVector(), DenseMatrix.zeros[Double](1,1), DenseMatrix.zeros[Double](1,1))
 
 }
--- src/main/scala-2.11/com/experticity/hmm/Main.scala
+++ src/main/scala-2.11/com/experticity/hmm/Main.scala
@@ -1,6 +1,6 @@
 package com.experticity.hmm
 
-import com.experticity.hmm.HiddenMarkovModel.{Observation, State}
+import breeze.linalg.{DenseMatrix, DenseVector}
 
 /**
   * Created by josh.monson on 4/13/16.
@@ -8,21 +8,55 @@ import com.experticity.hmm.HiddenMarkovModel.{Observation, State}
 object Main {
   def main(args: Array[String]) {
 
-    val states = List[State](
-      'WantToLearn,
-      'WantToBuy
+    val numSequences = 10
+    val sequenceSize = 5
+
+    // Create the HMM we'll use to create data
+    val initial = DenseVector(0.38, 0.44, 0.18)
+    val transitions = DenseMatrix(
+      (0.4, 0.5, 0.1),
+      (0.2, 0.2, 0.6),
+      (0.25, 0.1, 0.65)
+    )
+    val emissions = DenseMatrix(
+      (0.73, 0.11, 0.16),
+      (0.32, 0.58, 0.1),
+      (0.3, 0.3, 0.4))
+    val hmmMaster = HiddenMarkovModel(HMMMeta(3, 3), initial, transitions, emissions)
+    val data = (1 to numSequences).map(_ => hmmMaster.generateSequence(sequenceSize)).toList
+
+
+    // Create the HMM we'll train
+    val hmm = HiddenMarkovModel(2, 2).copy(
+      initial = DenseVector(.85, .15),
+      transitions = DenseMatrix((.3, .7), (.1, .9)),
+      emissions = DenseMatrix((.4, .6), (.5, .5))
     )
 
-    val observations = List[Observation](
-      'Profile,
-      'Training,
-      'Store
-    )
+    val obs = List(List(0,1,1,0), List(1,0,1))
+    obs.foreach(seq => {
+      val h = hmm.withSequence(seq)
+      val a = h.alpha()
+      val b = h.beta()
+      println(h.gamma(a, b).toString(10, 100))
+//      h.xi(a,b).foreach(m => println(m.toString(10, 100)))
+      println("====================================")
+    })
 
-    val sequence = List[Observation]('Profile, 'Profile, 'Training, 'Profile, 'Store)
 
-    val hmm = HiddenMarkovModel.initialGuess(states, observations).withSequence(sequence)
-    println(hmm)
+
+
+//    data.map(_._2).foreach(d => {
+//      println(hmm)
+//      hmm = hmm.withSequence(d).emStep()
+//    })
+
+    //    (1 to 10).foreach(_ => {
+    //      println(hmm)
+    //      println("======================================================")
+    //      hmm = hmm.emStep()
+    //    })
+    //    println(hmm)
 
   }
 }
